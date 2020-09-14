63dbaab2eac502dbe2d78d07327bb033de49a820
DawidS <dawid.syty1@gmail.com>
2020-09-14 15:56:46 +0200

    fixed schedule tasks

--- detector/tasks.py
+++ detector/tasks.py
@@ -9,14 +9,14 @@ from .fetcher import get_data
 from .api_fbchat import send_message
 
 
-@periodic_task(run_every=timedelta(seconds=600))
+@periodic_task(run_every=crontab(hour=15, minute=31))
 def task_us_get_data():
     time = datetime.now().time()
     for item in ActionSettings.objects.filter(enable=True, time_from__lte=time, time_to__gte=time):
         get_data(item.symbol, item.token, item.time_resolution)
 
 
-@periodic_task(run_every=crontab(hour=15, minute=31))
+@periodic_task(run_every=timedelta(seconds=600))
 def task_parse_data():
     time = datetime.now().time()
     for item in ActionSettings.objects.filter(enable=True, time_from__lte=time, time_to__gte=time):
