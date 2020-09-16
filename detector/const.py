768174c3135681e121bf1633d414b97f3a020664
DawidS <dawid.syty1@gmail.com>
2020-09-16 11:18:21 +0200

    change the const settings

--- detector/const.py
+++ detector/const.py
@@ -5,4 +5,4 @@ DATETIME_FORMAT = '%Y-%m-%d %H:%M:00'
 BASE_URL_CANDLE = 'https://finnhub.io/api/v1/stock/candle'
 BASE_URL_QUOTE = 'https://finnhub.io/api/v1/stock/quote'
 
-HISTORICAL_DATA = True
+HISTORICAL_DATA = False
