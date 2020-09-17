86ca3d530c1a52774e2028141084d59af8385d7d
DawidS <dawid.syty1@gmail.com>
2020-09-17 15:24:40 +0200

    fixed time-resolution

--- detector/api_finnhub.py
+++ detector/api_finnhub.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from datetime import datetime, timedelta
 from .const import BASE_URL_CANDLE, BASE_URL_QUOTE, HISTORICAL_DATA
+from .models import StockType
 
 
 class REQUEST_PARAMETERS:
@@ -14,20 +15,23 @@ class REQUEST_PARAMETERS:
 
 
 def get_last_5_minutes_data(item):
-    from_date = (datetime.now() + timedelta(minutes=-int(item.resolution) * 2)).strftime('%s')
+    from_date = (datetime.now() + timedelta(minutes=-int(item.time_resolution) * 2)).strftime('%s')
     if HISTORICAL_DATA:
         from_date = (datetime.now() + timedelta(days=-1, minutes=0)).strftime('%s')
 
-    response = requests.get(
-        BASE_URL_CANDLE,
-        {
-            REQUEST_PARAMETERS.SYMBOL: item.symbol,
-            REQUEST_PARAMETERS.RESOLUTION: item.resolution,
-            REQUEST_PARAMETERS.FROM: from_date,
-            REQUEST_PARAMETERS.TO: datetime.now().replace(second=1).strftime('%s'),
-            REQUEST_PARAMETERS.TOKEN: item.token,
-        }
-    )
+    if item.stock_type == StockType.STOCK:
+        response = requests.get(
+            BASE_URL_CANDLE,
+            {
+                REQUEST_PARAMETERS.SYMBOL: item.symbol,
+                REQUEST_PARAMETERS.RESOLUTION: item.time_resolution,
+                REQUEST_PARAMETERS.FROM: from_date,
+                REQUEST_PARAMETERS.TO: datetime.now().replace(second=1).strftime('%s'),
+                REQUEST_PARAMETERS.TOKEN: item.token,
+            }
+        )
+    else:
+        raise NotImplemented
 
     serialized_response = {}
 
@@ -42,19 +46,21 @@ def get_last_30_days_data(item):
     to_date = (datetime.now() + timedelta(days=-1)).replace(second=0, hour=0, minute=1).strftime('%s')
     if HISTORICAL_DATA:
         to_date = (datetime.now() + timedelta(days=-2)).replace(second=0, hour=0, minute=1).strftime('%s')
-
-    response = requests.get(
-        BASE_URL_CANDLE,
-        {
-            REQUEST_PARAMETERS.SYMBOL: item.symbol,
-            REQUEST_PARAMETERS.RESOLUTION: item.resolution,
-            REQUEST_PARAMETERS.FROM: (
-                    datetime.now() + timedelta(days=-30)
-            ).replace(second=0, hour=0, minute=1).strftime('%s'),
-            REQUEST_PARAMETERS.TO: to_date,
-            REQUEST_PARAMETERS.TOKEN: item.token,
-        }
-    )
+    if item.stock_type == StockType.STOCK:
+        response = requests.get(
+            BASE_URL_CANDLE,
+            {
+                REQUEST_PARAMETERS.SYMBOL: item.symbol,
+                REQUEST_PARAMETERS.RESOLUTION: item.time_resolution,
+                REQUEST_PARAMETERS.FROM: (
+                        datetime.now() + timedelta(days=-30)
+                ).replace(second=0, hour=0, minute=1).strftime('%s'),
+                REQUEST_PARAMETERS.TO: to_date,
+                REQUEST_PARAMETERS.TOKEN: item.token,
+            }
+        )
+    else:
+        raise NotImplemented
 
     try:
         serialized_response = response.json()
@@ -66,13 +72,16 @@ def get_last_30_days_data(item):
 
 def get_last_data(item):
     serialized_response = {}
-    response = requests.get(
-        BASE_URL_QUOTE,
-        {
-            REQUEST_PARAMETERS.SYMBOL: item.symbol,
-            REQUEST_PARAMETERS.TOKEN: item.token,
-        }
-    )
+    if item.stock_type == StockType.STOCK:
+        response = requests.get(
+            BASE_URL_QUOTE,
+            {
+                REQUEST_PARAMETERS.SYMBOL: item.symbol,
+                REQUEST_PARAMETERS.TOKEN: item.token,
+            }
+        )
+    else:
+        raise NotImplemented
 
     try:
         serialized_response = response.json()
