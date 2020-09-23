24b260f69689495a06659bdcdd8d4a52abb821c7
DawidS <dawid.syty1@gmail.com>
2020-09-23 13:14:07 +0200

    added simple view for home page

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', include('detector.urls')),
 ]
--- detector/views.py
+++ detector/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from .models import BearDetect
+from django.db.models import Count
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class BearListView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'bear_detector.html'
+
+    @staticmethod
+    def create_context_for_item():
+        queryset = BearDetect.objects.all()
+        return [
+            {
+                'name': item['name'],
+                'count': item['c'],
+                'link': f'https://www.etoro.com/markets/{item["name"]}/chart'
+            }
+            for item in queryset.values('name').annotate(c=Count('name')).order_by('-c')
+        ]
+
+    @staticmethod
+    def create_bear_list():
+        return [
+            {
+                'name': item.name,
+                'symbol': item.symbol,
+                'time': item.time,
+            }
+            for item in BearDetect.objects.all().order_by('-time')[:20]
+        ]
+
+    def get(self, request):
+        return Response({
+            'bears_list': self.create_bear_list(),
+            'most_active_items': self.create_context_for_item()
+        })
